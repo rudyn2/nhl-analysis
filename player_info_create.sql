e8afdf29dc57749cda2faaf6c89dba128b27217a
Rudy <c01010010@hotmail.com>
2020-11-25 16:04:46 -0300

    add player info create script

---
+++ player_info_create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE nhl.player_info
+(
+    player_id bigint NOT NULL,
+    firstName varchar(255),
+    lastName varchar(255),
+    nationality varchar(255),
+    birthCity varchar(255),
+    primaryPosition varchar(255),
+    birthDate date,
+    link varchar(255),
+    PRIMARY KEY (player_id)
+)
+TABLESPACE pg_default;
+
+ALTER TABLE nhl.game
+    OWNER to cc3201;
